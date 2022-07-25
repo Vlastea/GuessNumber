9ade7fb5a421181106ffe3aa54e576db5b47aa7a
Vlas <vlastea@yandex.ru>
2022-07-25 12:37:14 +0300

    GuessNumber
    Guess Number game

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the highest number in the range: ");
+            int maxNum = int.Parse(Console.ReadLine());
+            Random rand = new Random();
+            int number = rand.Next(1, maxNum);
+            string ent;
+            int entNum;
+            int retries = 0;
+
+            Console.WriteLine($"\nLet's try! Guess the random number from 1 to {maxNum} by entering a number that you wanted. If you want to end the game, press \"Enter\"\n");
+            while (true)
+            {
+                Console.Write(" Enter the number: ");
+                ent = Console.ReadLine();
+                retries++;
+
+                if (ent != "")
+                {
+                    entNum = int.Parse(ent);
+
+                    if (entNum < number)
+                    {
+                        Console.Write("The number is smaller.");
+                    }
+
+                    else if (entNum > number)
+                    {
+                        Console.Write("The number is bigger.");
+                    }
+
+                    else if (entNum == number)
+                    {
+                        Console.WriteLine($"\nYou won! The number is {number}");
+                        break;
+                    }
+
+                }
+
+                else
+                {
+                    Console.WriteLine($"The number is {number}");
+                    break;
+                }
+            }
+            Console.WriteLine($"Number of retires = {retries}");
+            Console.ReadLine();
+        }
+    }
+}
